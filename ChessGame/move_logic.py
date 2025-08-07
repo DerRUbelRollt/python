d7c67f65184cf56f5e3beb701858fb36607d626d
Max <maxrubel8@gmail.com>
2025-08-07 22:16:41 +0200

    turm + läufer logik/ pferd logik muss noch gemacht werden

--- ChessGame/move_logic.py
+++ ChessGame/move_logic.py
@@ -11,11 +11,9 @@ def get_pawn_moves(board, row, col, is_white, is_black):
         # Prüfen, ob das Feld vor dem Bauern leer ist
         if board[row - 1][col] == "":
             moves.append((row - 1, col))
-            print(f"Pawn move added: from ({row}, {col}) to ({row - 1}, {col})")
         # Anfangszug 2 Felder
         if row == 6 and board[row - 2][col] == "":
             moves.append((row - 2, col))
-            print(f"Pawn move added: from ({row}, {col}) to ({row - 2}, {col})")
             # Prüfen, ob das Feld schräg links oder rechts vom Bauern eine gegnerische Figur hat
         for r in rank: 
             if col != 7: 
@@ -48,42 +46,82 @@ def get_pawn_moves(board, row, col, is_white, is_black):
 
 def get_rook_moves(board, row, col, is_white, is_black):
     moves = []
+    # Richtungen Oben / unten/ links/ rechts
+    directions = [(-1,0), (1,0), (0,-1), (0,1)]
     # Die Range in der sich der sich alle Türem bewegen dürfen
-    for step in range(1, 8):
-        new_row_up = row - step
-        new_col_left = col - step
-        new_row_down = row + step
-        new_col_right = col + step
-        if new_row_up < 0 :
-            new_row_up = 0         # außerhalb des Bretts
-        if new_col_left < 0 :
-            new_col_left = 0       # außerhalb des Bretts
-        if new_row_down > 7 :
-            new_row_down = 7       # außerhalb des Bretts
-        if new_col_right > 7 :
-            new_col_right = 7      # außerhalb des Bretts
-        # Von unten zu oben wenn keine Figuren im weg sind
-        if row > new_row_up and board[new_row_up][col] == "":
-            moves.append((new_row_up, col))
-        # Von rechts nach links wenn keine Figuren im weg sind
-        if col > new_col_left and board[row][new_col_left] == "" :
-            moves.append((row, new_col_left))
-        # Von oben zu unten wenn keine Figuren im weg sind
-        if row < new_row_down and board[new_row_down][col] == "" :
-            moves.append((new_row_down, col))
-        # Von links nach rechts wenn keine Figuren im weg sind
-        if col < new_col_right and board[row][new_col_right] == "" :
-            moves.append((row, new_col_right))
-
+    for dr, dc in directions:
+        for step in range(1, 8):
+            new_row = row + dr * step
+            new_col = col + dc * step
+            if new_row < 0 or new_row > 7 or new_col < 0 or new_col > 7:
+                break
+            target = board[new_row][new_col]
+            if target == "":
+                moves.append((new_row, new_col))
+            else:
+                if is_white and target.startswith("b") or is_black and target.startswith("w"):
+                    moves.append((new_row, new_col))
+                    break # danach NICHT weitergehen
+                break
+             
          
     return moves    
 
 def get_knight_moves(board, row, col, is_white, is_black):
     moves = []
+    directions_L_R = [(0,-1), (0,1)]
+    directions_O_D = [(-1,0), (1,0)]
+    for dr, dc in directions_L_R:
+        col_up_down = col + dc * 1
+        row_left_right = row + dr * 1
+        row_up = row - 2
+        row_down = row + 2
+        col_left = col - 2
+        col_right = col + 2
+        if col_up_down > 7:
+            col_up_down = 7
+        if row_left_right > 7:
+            row_left_right = 7
+        if row_up < 0:
+            row_up = row 
+        if row_down > 7:
+            row_down = row
+        if col_left < 0 :
+            col_left = col
+        if col_right > 7:
+            col_right = col
+        if (is_white and not board[row_up][col_up_down].startswith("w")) or (is_black and not board[row_up][col_up_down].startswith("b")):
+            moves.append((row_up, col_up_down))
+
+        if (is_white and not board[row_down][col_up_down].startswith("w")) or (is_black and not board[row_down][col_up_down].startswith("b")):
+            moves.append((row_down, col_up_down))
+            
+        if (is_white and not board[row_left_right][col_left].startswith("w")) or (is_black and not board[row_left_right][col_left].startswith("b")):
+            moves.append((row_left_right, col_left))
+
+        if (is_white and not board[row_left_right][col_right].startswith("w")) or (is_black and not board[row_left_right][col_right].startswith("b")):
+            moves.append((row_left_right, col_right))
+        
     return moves
 
 def get_bishop_moves(board, row, col, is_white, is_black):
     moves = []
+        # Richtungen Diagonal oben links/ oben rechts/ unten rechts/ unten links
+    directions = [(-1,-1), (-1,1), (1,1), (1,-1)]
+    for dr, dc in directions:
+        for step in range(1,8):
+            new_row = row + dr * step
+            new_col = col + dc * step
+            if new_row < 0 or new_row > 7 or new_col < 0 or new_col > 7:
+                break
+            target = board[new_row][new_col]
+            if target == "":
+                moves.append((new_row, new_col))
+            else:
+                if is_white and target.startswith("b") or is_black and target.startswith("w"):
+                    moves.append((new_row, new_col))
+                    break # danach NICHT weitergehen
+                break
     return moves
 
 def get_queen_moves(board, row, col, is_white, is_black):
